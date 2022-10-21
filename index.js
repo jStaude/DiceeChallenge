b4116b2f80d2670db451644542900644c82f2643
Rupert <joel.staude1@gmail.com>
2022-10-21 14:47:58 -0500

    All files

---
+++ index.js
@@ -0,0 +1,16 @@
+var num1 = Math.floor(Math.random()*6) + 1;
+var num2 = Math.floor(Math.random()*6) + 1;
+
+ document.querySelector(".dice .img1").setAttribute("src","images/dice" + num1 + ".png");
+ document.querySelector(".dice .img2").setAttribute("src","images/dice" + num2 + ".png");
+
+
+if(num1>num2){
+  document.querySelector("h1").innerHTML ="Player 1 Wins!";
+}
+else if(num2>num1){
+  document.querySelector("h1").innerHTML ="Player 2 Wins!";
+}
+else{
+  document.querySelector("h1").innerHTML ="Draw";
+}
